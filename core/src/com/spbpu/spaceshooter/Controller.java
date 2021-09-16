3213573e74b55b26c6b60bdab503e1f7c64ff652
voviz <zaharov.va@edu.spbstu.ru>
2021-09-16 19:44:17 +0300

    1232

---
+++ core/src/com/spbpu/spaceshooter/Controller.java
@@ -0,0 +1,58 @@
+package com.spbpu.spaceshooter;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Controller {
+    public static void detectInput(float deltaTime, PlayerShip playerShip, float WORLD_WIDTH, float WORLD_HEIGHT, Viewport viewport) {
+        float leftLimit, rightLimit, upLimit, downLimit;
+        leftLimit = -playerShip.boundingBox.x;
+        downLimit = -playerShip.boundingBox.y;
+        rightLimit = WORLD_WIDTH - playerShip.boundingBox.x - playerShip.boundingBox.width;
+        upLimit = WORLD_HEIGHT /2 - playerShip.boundingBox.y - playerShip.boundingBox.height;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && rightLimit > 0) {
+            playerShip.translate(Math.min(playerShip.movementSpeed * deltaTime, rightLimit), 0f);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && upLimit > 0) {
+            playerShip.translate(0f, Math.min(playerShip.movementSpeed * deltaTime, upLimit));
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && leftLimit < 0) {
+            playerShip.translate(Math.max(-playerShip.movementSpeed * deltaTime, leftLimit),0f);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && downLimit < 0) {
+            playerShip.translate(0f, Math.max(-playerShip.movementSpeed * deltaTime, downLimit));
+        }
+
+        if (Gdx.input.isTouched()) {
+            float xTouchPixels = Gdx.input.getX();
+            float yTouchPixels = Gdx.input.getY();
+
+            Vector2 touchPoint = new Vector2(xTouchPixels, yTouchPixels);
+            touchPoint = viewport.unproject(touchPoint);
+
+            Vector2 playerShipCentre = new Vector2(
+                    playerShip.boundingBox.x + playerShip.boundingBox.width / 2,
+                    playerShip.boundingBox.y + playerShip.boundingBox.height /2
+            );
+            float touchDistance = touchPoint.dst(playerShipCentre);
+
+            if (touchDistance > 0.5f) {
+                float xTouchDifference  = touchPoint.x - playerShipCentre.x;
+                float yTouchDifference  = touchPoint.y - playerShipCentre.y;
+
+                float xMove = xTouchDifference / touchDistance * playerShip.movementSpeed * deltaTime;
+                float yMove = yTouchDifference / touchDistance * playerShip.movementSpeed * deltaTime;
+
+                if (xMove > 0) xMove = Math.min(xMove, rightLimit);
+                else xMove = Math.max(xMove, leftLimit);
+                if (yMove > 0) yMove = Math.min(yMove, upLimit);
+                else yMove = Math.max(yMove, downLimit);
+
+                playerShip.translate(xMove, yMove);
+            }
+        }
+    }
+}
